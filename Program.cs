2eb17715dbec82c464cbb8a7ee5d45f0a47b0702
seynyy <zenia.zt2006@gmail.com>
2024-09-06 11:50:03 +0300

    Update 4

--- Program.cs
+++ Program.cs
@@ -1,27 +1,33 @@
-﻿namespace lab1
+﻿using System.Text;
+
+namespace lab1
 {
     class Program {
         static void Main(string[] args) {
+            Console.OutputEncoding = Encoding.UTF8;
+
             //task1
             string firstName = "Євгеній";
             string lastName = "Боримський";
             int age = 17;
 
-            Console.Write($"Мене звати {firstName} {lastName}, мені {age} років.");
+            Console.WriteLine($"Мене звати {firstName} {lastName}, мені {age} років.");
+            Console.WriteLine("-------------------------");
             Console.Read();
-            
+
             //task2
             int a = 3;
             int b = 1;
 
-            float result = ((a+b)^2-a^2+2*a*b)/b^2;
+            double result = (Math.Pow((a+b), 2f) - Math.Pow(a,2) + 2*a*b) / Math.Pow(a,2);
 
             Console.Write(result);
+            Console.WriteLine("-------------------------");
             Console.Read();
 
             //task3
-            int number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(number.ToString().Reverse());
+            string number = Console.ReadLine();
+            Console.WriteLine(number.Reverse());
         }
     }
 }
