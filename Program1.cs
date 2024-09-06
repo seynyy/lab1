26dce3f5d2b95ecf6b5601a0c00c9cf818ffbd74
seynyy <zenia.zt2006@gmail.com>
2024-09-06 11:05:02 +0300

    Update

--- Program1.cs
+++ Program1.cs
@@ -2,12 +2,26 @@
 {
     class Program1 {
         static void Main(string[] args) {
-            string _firstName = "Євгеній";
-            string _lastName = "Боримський";
-            int _age = 17;
+            //task1
+            string firstName = "Євгеній";
+            string lastName = "Боримський";
+            int age = 17;
 
-            Console.Write("Мене звати {0} {1}, мені {2} років.", _firstName, _lastName, _age);
+            Console.Write("Мене звати {0} {1}, мені {2} років.", firstName, lastName, age);
             Console.Read();
+            
+            //task2
+            int a = 3;
+            int b = 1;
+
+            float result = ((a+b)^2-a^2+2*a*b)/b^2;
+
+            Console.Write(result);
+            Console.Read();
+
+            //task3
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number.ToString().Reverse());
         }
     }
 }
--- obj/Debug/net8.0/lab1.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd8b32da411c2751b2e6724fc227f24eb4959c98")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
